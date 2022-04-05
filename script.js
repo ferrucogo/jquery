c10d280df8eb19c393723b5e7b615769f7a4f994
ferrucogo <43414055+ferrucogo@users.noreply.github.com>
2022-04-05 11:55:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 $(document).ready(() => {
-    //console.log('ciao');
-    // $('#ciao').html('pluto'); // $('#ciao') = document.getElementById('ciao'). Si usa #, come per leggere gli id nel css.
-    // $('p').html('pluto'); // Chiamata per tag, in questo caso il tag p.
-    // $('.container p').html('pluto'); // Prendo i tag p dentro al tag con classe container.
-    // $('p').on('click', function() {
-    //     $('p').hide();
-    // });
-
     $.getJSON("./assets/array.json", function (data) { // Leggo il file json
         $.each(data, function (key, val) { // Per ogni elemento
             $("<p>", { // Creo il tag p, dandogli una classe e un contenuto, e lo appendo a un div contenitore
@@ -14,20 +6,6 @@ $(document).ready(() => {
                 html: val
             }).appendTo(".container");
 
-            // Dopo aver creato l'elemento imposto mouseover e mouseout, dando come parametro la classe corrispondente. Imposto anche il click per farli scomparire.
-            // $("." + val).on("mouseover", function () {
-            //     $("." + val).css("color", "orange");
-            // });
-    
-            // $("." + val).on("mouseout", function () {
-            //     $("." + val).css("color", "black");
-            // });
-
-            // $("." + val).on("click", function () {
-            //     $("." + val).hide();
-            // });
-
-            // Dopo aver creato l'elemento imposto mouseover e mouseout, dando come parametro il testo dell'elemento appena creato. Imposto anche il click per farli scomparire.
             $("p:contains(" + val + ")").on("mouseover", function () {
                 $("p:contains(" + val + ")").css("color", "orange");
             });
@@ -44,13 +22,3 @@ $(document).ready(() => {
     });
 
 });
-
-
-// window.addEventListener('load', () => {
-//     //console.log('ciao in javascript');
-//     //document.getElementById('ciao').innerHTML = 'pippo';
-//     //console.log('vanilla', document.getElementsByTagName('p'));
-//     //[...document.getElementsByTagName('p')].forEach(e => e.innerHTML = 'paperino');
-//     document.getElementById('ciao').className = 'ciao';
-//     document.getElementById('ciao').classList.add('mondo');
-// });
